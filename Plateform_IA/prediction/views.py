eee117b109c62d2a0fd6ad9f6f44cdbf62236a41
ayoubaoubid <ayoubaoubid3@gmail.com>
2026-04-03 14:53:38 +0100

    plateform

--- Plateform_IA/prediction/views.py
+++ Plateform_IA/prediction/views.py
@@ -5,6 +5,14 @@ import random
 def ml_model(request):
     return render(request, 'prediction/ml_model.html')
 
+import pandas as pd
+import numpy as np
+
+def data_set(data) :
+    
+    
+    return dataset
+
 def prediction(request):
     if request.method == 'POST':
         # Dummy prediction logic
