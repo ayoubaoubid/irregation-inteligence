4507366b72241d4f907ed75069181fa3565db655
ayoubaoubid <ayoubaoubid3@gmail.com>
2026-04-19 06:48:48 +0100

    DevOps

--- Plateform_IA/dataset/urls.py
+++ Plateform_IA/dataset/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dataset, name='dataset'),
     path('analysis/', views.analysis, name='analysis'),
+    path('add-data/', views.add_data, name='add_data'),
 ]
--- Plateform_IA/smart_irrigation/settings.py
+++ Plateform_IA/smart_irrigation/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'core',
     'dataset',
     'prediction',
-    'dashboard',
 ]
 
 MIDDLEWARE = [
