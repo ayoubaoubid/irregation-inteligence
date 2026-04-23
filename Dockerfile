58c7e8ba7ce7e61bda2b69e22e8a149625910cdf
ayoubaoubid <ayoubaoubid3@gmail.com>
2026-04-23 13:11:59 +0100

    testing

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM python:3.10
+FROM python:3.12
 
 # working directory
 WORKDIR /app
 
 # install dependencies
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt --timeout 300
 
 # copy project
 COPY . .
 
 # expose FastAPI port
-EXPOSE 8000
+EXPOSE 5000
 
 # run API
-CMD ["uvicorn", "scripts.predict:app", "--reload"]
+CMD ["uvicorn", "scripts.predict:app", "--host", "0.0.0.0", "--port", "5000"]
--- Plateform_IA/dataset/views.py
+++ Plateform_IA/dataset/views.py
@@ -1,7 +1,40 @@
+import csv
+import os
 from django.shortcuts import render
+from django.conf import settings
 
 def dataset(request):
     return render(request, 'dataset/dataset.html')
 
 def analysis(request):
     return render(request, 'dataset/analysis.html')
+
+def add_data(request):
+    if request.method == 'POST':
+        data = [
+            request.POST.get('Soil_pH'),
+            request.POST.get('Soil_Moisture'),
+            request.POST.get('Organic_Carbon'),
+            request.POST.get('Electrical_Conductivity'),
+            request.POST.get('Temperature_C'),
+            request.POST.get('Humidity'),
+            request.POST.get('Rainfall_mm'),
+            request.POST.get('Sunlight_Hours'),
+            request.POST.get('Wind_Speed_kmh'),
+            request.POST.get('Crop_Growth_Stage'),
+            request.POST.get('Irrigation_Type'),
+            request.POST.get('Field_Area_hectare'),
+            request.POST.get('Mulching_Used'),
+            request.POST.get('Previous_Irrigation_mm'),
+            request.POST.get('Irrigation_Need')
+        ]
+        
+        # Adding to the static CSV
+        csv_path = r'd:\MLOPS_env\irregation-inteligence\DataOps\Statics\irrigation_prediction_Variables_Important.csv'
+        with open(csv_path, mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+            
+        return render(request, 'dataset/add_data.html', {'success': True})
+        
+    return render(request, 'dataset/add_data.html')
